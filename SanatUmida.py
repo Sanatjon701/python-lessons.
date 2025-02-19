eea6b4f691c3162ac029825f1c5ec83fb843f101
Sanatjon701 <shavkatovsanat775@gmail.com>
2025-02-19 15:23:59 +0500

    Add files via upload

---
+++ SanatUmida.py
@@ -0,0 +1,206 @@
+#cars = ['Nexia 3', 'Malibu', 'Gentra', 'Tracker 2']
+#fruit = ['apple', 'banana', 'orange', 'uzum', 'cherry']
+#prices = [12200, 13200, 17800, 24000, 22500]
+
+#thelistoflover = ('Umida', 'Shaxnoz', 'Aziza', 'Shaxina', )
+#for umide in thelistoflover:
+ #   print("Salom", umide)
+#    print("hayr", umide)
+
+
+
+#for  umide in thelistoflover:
+#    print(f"Assalomu Alaykum {umide} sizni 17 avgust kuni Sanatjon va Umida tuylariga taklif qilaman")
+#    print("Hurmat bilan Sanatjon")
+          
+
+#number = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#for num in number:
+#    print(f"{num} ning kvadrati {num**3} ga teng")
+
+
+#dostlar = []
+#print("5 ta eng yaqin dostingiz kim")
+#for soni in range(5):
+#    dostlar.append (input(f"{soni+1}-do'stingizni ismini kiriting:"))
+#    print(dostlar)
+
+
+#javob=float(input("13x5 nechchiga teng?"))
+#if javob!=65:
+#   print("Javob xato!")
+#else: 
+#   print("Javobingiz to\'g\'ri")
+
+#age = int(input("Yoshingiz nechchida?>>>"))
+#if age>=18:
+#    print("Xush kelibsiz")
+#else:
+#    print("Kirish mumkin emas!")
+
+
+#age = int(input("Tug'ilgan yilingizni kiriting!:"))
+#if 2025-age<18:
+#    print(f"Yoshingiz {2025-age} ekan")
+#    print("Kirish mumkin emas!")
+#else:
+#    print("Xush kelibsiz!")    
+
+
+#age = int(input("Yoshingiz nechida?:"))
+#if age>65:
+#    print(f"Siz {2025-age} ekansiz, uje qaridingiz damizni oling otaxon!")
+#else:
+#    print("San pizdukcha kirsang bo'ladi")
+
+
+#age = int(input("Hayvonot bog\'imizga xush kelibsiz! Yoshingiz nechchida okajonim?>>>"))
+#if age<4:
+#    print("Sizga kirish 5000 so\'m")
+#elif age<12:
+#    print("Sizga kirish 8000 so\'m")
+#elif age<18:
+#    print("San sherichaga kirish 1.000.000 dollar, buvingni ami")
+#elif age>18:
+#    print("Sanlar kotta uje naxxuy sanlarni hayvon ko\'rsa sikvorishi mumkin!")    
+    
+
+#age = int(input('Yoshingiz nechchida opoqi? '))
+#if age<4:
+#    narx = 0
+#elif age<=12:
+#    narx = 5000
+#elif age<=18:
+#    narx = 8000
+#elif age<=28:
+#    narx = 10000
+#elif age>=29:
+#    narx = 20000    
+#else:
+#    narx = 30000
+    
+    
+#print(f"Sizga kirish {narx} so\'m")
+    
+
+#day = input("Bugun qaysi hafta kuni?>>>")
+#if day.lower()=='shanba' or day.lower()=='yakshanba':
+#    print("Bugun dam olish kuni")
+#else:
+#    print("Bugun ish kuni!")    
+
+
+#day=input("Bugun qaysi kun? ")
+#temperature=float(input("Havo harorati qanday? "))
+#if (day.lower()=='shanba' or day.lower()=='yakshanba') and temperature>=30:
+#    print("Cho'milgani ketdik")    
+#elif (day.lower()=='shanba' or day.lower()=='yakshanba') and temperature<=30:
+#    print("Uyda dam olamiz!")    
+#else:
+#    print("Bugun ish kuniku axmoq")    
+
+#narx = 15000
+#tea = True
+#bread = True 
+#kebab = True
+#sweets = False
+
+#if tea:
+#    print("Mijoz choy sotib oldi")
+#    narx = narx + 7000
+#if bread:
+#    print("Mijoz non sotib oldi")
+#    narx = narx + 5000
+#if kebab:
+#    print("Mijoz shashlik sotib oldi")
+#    narx = narx + 20000
+ 
+#    print(f"Jami {narx} so\'m")    
+    
+
+
+#menu = ['shashlik', 'halisa', 'manti', 'somsa', 'osh', 'shurva', 'xonim']
+#food = input("Nima ovqat yeysiz?>>>")
+#if food.lower() in menu:
+#    print("Xo'p bo'ladi opajonim, buyurtmangiz qabul qilindi")
+#else:
+#    print("Kechirasiz siz sichqondek ovqat tanlar ekansiz, yo'q siz aytgan narsa!")    
+
+
+
+number = input("Istalgan sonni kiriting>>>")
+number = int(number)
+print(f"{number} ning kvadrati {number**2} ga teng.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
